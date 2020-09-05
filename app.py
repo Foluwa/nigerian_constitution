3dfeea8d5271b68b453852766ece946fd102b52f
Foluwa <moronfoluwaakintola@gmail.com>
2020-09-05 04:41:59 +0100

    sockets setup

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/')
 def hello():
-    return 'Hii Hello, World!'
-
+    return 'Conversation AI'
 
 @app.route('/chat')
 def chat():
     return render_template('chat.html')
+
+@socketio.on('my event')
+def test_message(message):
+    emit('my response', {'data': 'got it!'})
+
+if __name__ == '__main__':
+    socketio.run(app)
---
+++ setup.py
@@ -0,0 +1,5 @@
+"""
+
+"""
+import re
+from setuptools import setup
