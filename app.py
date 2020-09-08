59eed19d309f3653aab29cc7c4ab064a719c1571
Foluwa <moronfoluwaakintola@gmail.com>
2020-09-08 07:51:22 +0100

    Added sockets response

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, disconnect
 from threading import Lock
-
-# Set this variable to "threading", "eventlet" or "gevent" to test the
-# different async modes, or leave it set to None for the application to choose
-# the best option based on installed packages.
 async_mode = None
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, async_mode=async_mode)
+socketio = SocketIO(app, engineio_logger=True, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
 
@@ -32,11 +28,11 @@ def on_connect(self):
 def test_message(message):
     print('message is >> ', message)
     emit('my_response',
-         {'data': 'data is received!!'})
+         {'data': message})
 
 def on_disconnect(self):
-        print('Client disconnected')
+    print('Client disconnected')
         
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, threaded=True)
