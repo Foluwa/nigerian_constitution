6a7cd1a211034cca0f13e9272351b1c10b2fac4e
Foluwa <moronfoluwaakintola@gmail.com>
2020-09-19 11:35:30 +0100

    Implemnting sockets disconnect

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from gevent import monkey
 monkey.patch_all()
 from flask import Flask, render_template
@@ -7,7 +9,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 app.config['DEBUG'] = True
 # socketio = SocketIO(app, cors_allowed_origins="*", message_queue='redis://', async_mode='eventlet')
-socketio = SocketIO(app, async_mode="threading") # async_mode=None, logger=True, engineio_logger=True)
+socketio = SocketIO(app, async_handlers=True) # async_mode=None, logger=True, engineio_logger=True)
 
 @app.route('/')
 def index():
@@ -23,13 +25,15 @@ def on_connect(self):
 @socketio.on('my_event', namespace='/chat')
 def user_message(message):
     print('message is >> ', message)
-    emit('my_response', {'data': message})
+    mymessage = str(message)
+    emit('my_response', {'data': message })
+    # gevent.sleep(1)
     # socketio.sleep(2)
-    socketio.disconnect()
+    # socketio.disconnect()
 
 def on_disconnect(self):
     print('Client disconnected')
         
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True)
